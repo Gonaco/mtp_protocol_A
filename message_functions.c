235a03668e1df3d8771b786e8d54b2382241cc3a
Gonaco <danielmoremanza@gmail.com>
2017-10-15 20:16:33 +0200

    Some code and pseudocode for the message functions. How to work with the message string?

--- message_functions.c
+++ message_functions.c
@@ -1,19 +1,42 @@
-#include <stdlib.h>
-#include <stdio.h>
 
-#include <string.h>
+#include "message_functions.h"
 
-#include "libraries/RF24/src/rf24.h"
-
-bool send(unsigned char message[]){
+bool send(unsigned char frame[]){
 
   bool is_sent;
-  uint8_t length_bytes = strlen(message)*sizeof(unsigned char);
-  is_sent = rf24_write_payload(&message, length_bytes); //rf24_write or rf24_write_payload ?
+  uint8_t length_bytes = strlen(frame)*sizeof(unsigned char); //Shouldn't we use a uintblablabla bigger?
+  is_sent = rf24_write_payload(&frame, length_bytes); //rf24_write or rf24_write_payload ?
 
   return is_sent;
 
 }
 
+void frameAssembly(struct header head, unsigned char message[]){
+
+  unsigned char *frame;
+  // int crc = crcCalculation(); 
+  
+  // frame = head + message concatenation
+
+}
+
+bool sendData(unsigned char message[]){
+
+  struct header dataHead;
+  dataHead.signature = A_TEAM;
+  // dataHead.id = packetIdCalc(); // Function that follows the number of packets sent and gives you the next one
+  dataHead.type = 0b11;
+  // dataHead.padding = needsPadding(message);
+  
+
+  frame = frameAssembly(dataHead, message);
+
+  return send(frame);
+
+}
+
+
+
+// NACHO, ESCRIBE A PARTIR DE ESTA LÍNEA. PARA EVITAR COSAS RARAS EN LA FORMA DE PROCESAR CAMBIOS DE GITHUB
 
 
