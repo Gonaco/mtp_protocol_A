a09e77e9d9f4b70e40d5d77a25d8d000ef8bf770
danielmoremanza@gmail.com <danielmoremanza@gmail.com>
2017-10-22 15:18:53 +0000

    .

--- message_test.c
+++ message_test.c
@@ -2,6 +2,16 @@
 
 int main(){
 
+  rf24_init_radio("/dev/spidev0.0", 8000000, 25);
+  rf24_setRetries(15,15);
+  rf24_setChannel(0x4c);
+  rf24_setPALevel(RF24_PA_MAX);
+  /* rf24_openReadingPipe(1,pipes[1]); */
+  // en el codigo de ejemplo aparece pero no se si es necesario si se que lo primero que voy a hacer es enviar igual para saver details
+  rf24_startListening();
+  rf24_printDetails();
+  rf24_stopListening();
+
   unsigned char text[] = "Message from A-Team";
 
   printf("\n%s\n", text);
