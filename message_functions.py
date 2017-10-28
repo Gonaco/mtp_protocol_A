84795f13edf0ff07439940a0542e81b443aa873e
Gonaco <danielmoremanza@gmail.com>
2017-10-28 17:48:35 +0200

    starting to work with python

---
+++ message_functions.py
@@ -0,0 +1,11 @@
+
+class Header:
+    
+
+class Packet:
+    header = None
+    payload = None
+
+    def setHeader(self):
+        self.header = Header()
+        
---
+++ message_test.py
@@ -0,0 +1,9 @@
+import sys
+
+def main(argv):
+
+    # print ("\n"+argv[1])
+
+
+if __name__ == "__main__":
+    main(sys.argv)
