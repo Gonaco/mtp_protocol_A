c4cfea0a15425ba2a5d60614f94f382799ced2e3
Oliver Ploder <oliver.ploder@gmail.com>
2017-10-28 18:54:20 +0200

    worked on TX function

---
+++ tx_main_functions.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+from lib_nrf24 import NRF24
+import time
+import spidev
+
+
+
+pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]] #addresses for TX/RX channels
+
+radio = NRF24(GPIO, spidev.SpiDev())
+radio.begin(0, 17) # Set spi-cs pin0, and rf24-CE pin 17
+time.sleep(1)
+radio.setRetries(15,15)
+radio.setPayloadSize(32)
+radio.setChannel(0x60)
+
+radio.setDataRate(NRF24.BR_2MBPS)
+radio.setPALevel(NRF24.PA_MAX)
+radio.setAutoAck(True)
+radio.enableDynamicPayloads() # radio.setPayloadSize(32) for setting a fixed payload
+radio.enableAckPayload()
+
+
+radio.openWritingPipe(pipes[1])
+radio.openReadingPipe(1, pipes[0])
+radio.printDetails()
+
+paysize = 30 # size of payload we send at once
+
+##################DEBUG CODE BELOW############################
+run = True
+
+while run:
+    infile = open("tx_file.txt", “r”)
+    data = infile.read()
+    infile.close()
+
+    for i in range(0, len(data), paysize):
+        if (i+paysize) < len(data):
+            buf = data[i:i+paysize]
+        else:
+            buf = data[i:]
+            run = False
+
+        radio.write(buf)
+        print ("Sent:"),
+        print (buf)
+        # did it return with a payload?
+        if radio.isAckPayloadAvailable():
+            pl_buffer=[]
+            radio.read(pl_buffer, radio.getDynamicPayloadSize())
+            print ("Received back:"),
+            print (pl_buffer)
+        else:
+            print ("Received: Ack only, no payload")
+        time.sleep(1)
+
+print("Done sending the file! Exiting!")
