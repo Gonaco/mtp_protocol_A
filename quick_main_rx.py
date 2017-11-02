f176f6a29c0765e478ac5604fc9b2fcf3cf07c1b
Oliver Ploder <oliver.ploder@gmail.com>
2017-11-02 23:27:59 +0100

    Cleaned the directory

--- quick_main_rx.py
+++ quick_main_rx.py
@@ -3,17 +3,18 @@ GPIO.setmode(GPIO.BCM)
 from lib_nrf24 import NRF24
 import time
 import spidev
+import message_functions as m
 
 pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]] #addresses for TX/RX channels
 
 radio = NRF24(GPIO, spidev.SpiDev())
 radio2 = NRF24(GPIO, spidev.SpiDev())
-radio.begin(1, 17) # Set spi-cs pin1, and rf24-CE pin 27
-radio2.begin(0, 27) # Set spi-cs pin0, and rf24-CE pin 17
+radio.begin(1, 27) # Set spi-cs pin1, and rf24-CE pin 27
+radio2.begin(0, 17) # Set spi-cs pin0, and rf24-CE pin 17
 
 radio.setRetries(15,15)
 radio.setPayloadSize(32)
-radio.setChannel(0x60)
+radio.setChannel(0x65)
 radio2.setRetries(15,15)
 radio2.setPayloadSize(32)
 radio2.setChannel(0x60)
@@ -42,39 +43,52 @@ radio.startListening()
 
 c=1
 num=0
-outfile=open("rx_file.txt","w")
 run=True
+firstRun = True
 str = ""
+akpl_buf = [c,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8]
+pipe = [0]
+cnt = 0
 while run:
-    akpl_buf = [c,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8]
-    pipe = [0]
-    while not radio.available(pipe) and num<500:
-        time.sleep(10000/1000000.0)
-        num=num+1
-        if num ==499:
-            run=False
-    num=0
-    if run==True:
-        recv_buffer = []
-        radio.read(recv_buffer, radio.getDynamicPayloadSize())
-        print ("Received:")
-        recv_packet= m.Packet()
-        for i in range(0,len(recv_buffer),1):
-            str = str + chr(recv_buffer[i])
-        print (str)
-        recv_packet.strMssg2Pckt(str)
-	print(recv_packet)
-	print(recv_packet.getPayload())
-
-        c = c + 1
-        if (c&1) == 0:
-            ack=m.ACK(c, "")
-            ack.send(radio)
-            print ("Loaded payload reply:"),
-            print (akpl_buf)
-        else:
-            print ("(No return payload)")
+    cnt = cnt + 1
+    tmpStr = ""
+    
+    if firstRun == False:
+        while not radio.available(pipe) and num < 5000:
+            time.sleep(1/1000.0)
+            num=num+1
+        if num == 50000:
+            run = False
     else:
-        outfile.write(str)
-        outfile.close()
-        print("The message is received")
+        firstRun = False
+        while not radio.available(pipe):
+            time.sleep(1/1000.0)
+
+    num=0
+
+    recv_buffer = []
+    radio.read(recv_buffer, radio.getDynamicPayloadSize())
+    if cnt == 25:
+        print ("Received Packet!")
+
+    for i in range(0,len(recv_buffer),1):
+        tmpStr = tmpStr + chr(recv_buffer[i])
+
+    time.sleep(3/100.0) # wait a bit for processing
+
+    radio2.write(akpl_buf) #send ACK
+
+    if cnt == 25:
+        print ("ACK SENT")
+        cnt = 0
+
+    if tmpStr == "ThIs Is EnD oF FiLe...........":
+        run = False
+        print(tmpStr)
+    else:
+        str = str + tmpStr
+
+outfile=open("rx_file.txt","w")
+outfile.write(str)
+outfile.close()
+print("The message is received")
