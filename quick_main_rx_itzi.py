1f238edc2ee882454f77aa6eb9ed2b358f2df121
Oliver Ploder <oliver.ploder@gmail.com>
2017-11-02 18:17:19 +0100

    fixed imports

--- quick_main_rx_itzi.py
+++ quick_main_rx_itzi.py
@@ -3,7 +3,7 @@ GPIO.setmode(GPIO.BCM)
 from lib_nrf24 import NRF24
 import time
 import spidev
-
+import message_functions as m
 
 pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]] #addresses for TX/RX channels
 
