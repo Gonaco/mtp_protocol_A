103d645178adaa31f5aa9c23930667c55db626bb
Gonaco <danielmoremanza@gmail.com>
2017-11-03 21:59:50 +0100

    Quick Mode with Headers

--- message_functions.py
+++ message_functions.py
@@ -93,7 +93,7 @@ class Packet:
 
     def getPayload(self):
         ret = ""
-        for i in range(0, length(self.payload), 1):
+        for i in range(0, len(self.payload), 1):
             ret = ret + chr(self.payload[i])
         return ret
     
