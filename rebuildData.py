efaaa550cb783499639ce00c67ccd689a4482cda
Guillem-LC <guillem.lahuerta@hotmail.com>
2017-11-16 12:53:26 +0100

    Updated in order to fulfil Dani’s wishes
    Also modified name of variables
    The variable previously defined as “dict” has been named as “storedFrames”, the variable “id” is now “p_id”

--- rebuildData.py
+++ rebuildData.py
@@ -1,27 +1,29 @@
-dict = {-2 : "DEFAULT"}
+storedFrames = {-2 : "DEFAULT"}
 last_w_id = -1
 
-def rebuildData(id, string, last_w_id, dict, filename):
+def rebuildData(p_id, string, last_w_id, storedFrames, filename):
     
-    if (id == last_w_id + 1):
+    print "\n-rebuildData-\n"
+    
+    if (p_id == last_w_id + 1):
         ## El paquete recibido es el siguiente que tenemos que escribir
         writeFile(string, filename)
         last_w_id = last_w_id + 1
         ## Comprobamos si hay el paquete id+1 y posteriores en el diccionario
-        id = id + 1
-        while dict.has_key(id):
-            string = dict[id]
+        p_id = p_id + 1
+        while storedFrames.has_key(id):
+            string = storedFrames[p_id]
             writeFile(string, filename)
-            del dict[id]
+            del storedFrames[p_id]
             last_w_id = last_w_id + 1
-            id = id + 1
+            p_id = p_id + 1
         
-    elif (id > last_w_id):
+    elif (p_id > last_w_id):
         ## El paquete recibido es posterior al que tendriamos que escribir
         ## -> Lo añadimos en el diccionario
-        dict.update({id : string})
+        storedFrames.update({p_id : string})
         
     ## Devolvemos el diccionario y la last_w_id
-    return dict, last_w_id    
+    return storedFrames, last_w_id    
 
  
