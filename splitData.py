d2d435633143e3a9b7260bea47b1fc3345671884
carolinafdezp <32631342+carolinafdezp@users.noreply.github.com>
2017-11-16 14:35:23 +0100

    Update splitData.py

--- splitData.py
+++ splitData.py
@@ -1,16 +1,18 @@
-def splitData(nPacket):
+def splitData(PacketID):
 
-    archivo = open("RXFILE-D-MRM-4.txt")
+    print("\n-splitData-\n") ##Debbuging issues.
+	
+    archivo = open("RXFILE-D-MRM-4.txt") ## Example of a text file
 
     file_len = 10000 #Size of the file in bytes
     chunk_len = 30 #Size of the chunk in bytes
 
-    archivo.seek(nPacket*chunk_len) #It moves the pointer to the starting point of the chunk number 'nPacket'
+    archivo.seek(PacketID*chunk_len) #It moves the pointer to the starting point of the chunk number 'nPacket'
     chunk = archivo.read(chunk_len) #It reads 'cunk_len' bytes from the previous pointer
 
 
-    if nPacket>round((file_len/chunk_len)+1):
-        print("That block does not exist. EOF reached.")
+    if PacketID>round((file_len/chunk_len)+1):
+        print("That packet does not exist. EOF reached.")
 	
 
     return chunk
