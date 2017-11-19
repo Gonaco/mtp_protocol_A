c3b4a83142c7993c509ab2f458913f77300bd984
itziarsagas <itziar.sf@opendeusto.es>
2017-11-19 12:52:39 +0100

    tx modifications

--- tx_main_functions.py
+++ tx_main_functions.py
@@ -53,8 +53,10 @@ def setup():
 
     ears = NRF24(GPIO, spidev.SpiDev())
     mouth = NRF24(GPIO, spidev.SpiDev())
-    mouth.begin(1, 17)  # Set spi-cs pin1, and rf24-CE pin 27 ¿?SURE¿? NOT AS IN THE QUICK MODE
-    ears.begin(0, 27)  # Set spi-cs pin0, and rf24-CE pin 17 ¿?SURE¿? NOT AS IN THE QUICK MODE
+    mouth.begin(1, 27)  # Set spi-cs pin1, and rf24-CE pin 27 ¿?SURE¿? NOT AS IN THE QUICK MODE
+    ears.begin(0, 17)  # Set spi-cs pin0, and rf24-CE pin 17 ¿?SURE¿? NOT AS IN THE QUICK MODE
+
+    time.sleep(1)
 
     mouth.setRetries(15, 15)
     mouth.setPayloadSize(32)
@@ -169,32 +171,19 @@ def synchronized(radio, radio2, pipe):
     while not done:
         radio.write(sync.__str__())
         radio2.startListening()
-        # while not radio2.available(pipe) and num < 400: # WHY A TIMER (400) HERE?
-            # time.sleep(1 / 1000.0)
-            # num = num + 1
-        # if num != 400:
-            # print("we received something before time out")
-            # rcv_buffer = []
-            # radio2.read(rcv_buffer, radio2.getDynamicPayloadSize())
-            # rcv = m.Packet()
-            # rcv.mssg2Pckt(rcv_buffer)
-            # if rcv.getTyp() == 1:
-            #     if rcv.getID() == 0:
-            #         done = True
+        while not radio2.available(pipe) and num < 400: # WHY A TIMER (400) HERE?
+            time.sleep(1 / 1000.0)
+            num = num + 1
+        if num != 400:
+            print("we received something before time out")
+            rcv_buffer = []
+            radio2.read(rcv_buffer, radio2.getDynamicPayloadSize())
+            rcv = m.Packet()
+            rcv.mssg2Pckt(rcv_buffer)
+            if rcv.getTyp() == 1:
+                if rcv.getID() == 0:
+                    done = True
 
-        while not radio2.available(pipe):
-            # do nothing
-            pass
-            
-
-        print("we received something before time out")
-        rcv_buffer = []
-        radio2.read(rcv_buffer, radio2.getDynamicPayloadSize())
-        rcv = m.Packet()
-        rcv.mssg2Pckt(rcv_buffer)
-        if rcv.getTyp() == 1:
-            if rcv.getID() == 0:
-                done = True
 
 
 def end_connection(radio, radio2, pipe, last_id):
