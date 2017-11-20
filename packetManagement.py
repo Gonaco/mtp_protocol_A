9562ccc71a642603ec214c796744f587928e12d0
carolinafdezp <32631342+carolinafdezp@users.noreply.github.com>
2017-11-20 17:08:20 +0100

    Update packetManagement.py

--- packetManagement.py
+++ packetManagement.py
@@ -53,13 +53,15 @@ def splitData(PacketID, archivo):
 	
     file_len = len(archivo.read()) #Size of the file in bytes
     chunk_len = 30 #Size of the chunk in bytes
+    packets = file_len/chunk_len	
 
     archivo.seek(PacketID*chunk_len) #It moves the pointer to the starting point of the chunk number 'nPacket'
     chunk = archivo.read(chunk_len) #It reads 'cunk_len' bytes from the previous pointer
 
 
     if PacketID>round((file_len/chunk_len)+1):
-        print("That packet does not exist. EOF reached.")
+        print("That packet %d does not exist. EOF reached.", PacketID)
+	return packets
 	
 
     return chunk
