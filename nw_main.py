abe96e073f9bfd98de8666696606dac86fdbed8c
Gonaco <danielmoremanza@gmail.com>
2017-11-23 01:47:36 +0100

    testing NW Mode a lo bestia

--- nw_main.py
+++ nw_main.py
@@ -2,10 +2,16 @@ import Net_main_functions as nw
 
 ears, mouth = nw.setup()
 
-timer = 5
+# timer = 5
 
-while True:
-    if (nw.listen(ears, timer)):
-        nw.passive(ears,mouth)
-    else:
-        print("Timeout")
+# while True:
+#     if (nw.listen(ears, timer)):
+#         nw.passive(ears,mouth)
+#     else:
+#         print("Timeout")
+
+f = open("tx_file.txt", 'r')
+
+files = [f,f,f]
+
+nw.network_mode(ears,mouth,files)
