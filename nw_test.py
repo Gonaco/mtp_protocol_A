5259c20d51f280109524d2bfd98eafaef3d55527
Gonaco <danielmoremanza@gmail.com>
2017-11-24 11:44:59 +0100

    NW tests

--- nw_test.py
+++ nw_test.py
@@ -9,7 +9,48 @@ import random
 import Net_main_functions as nw
 import time
 
+# NETWORK CONSTANTS
+PAYLOAD_LENGTH = 31
+HEADER_LENGTH = 1
+TDATA_MAX = TACK_MAX = 0.2                         # Data and ACK frames timeout (in seconds)
+TDATA = TACK = 0.0005                                     # Waiting time in order to transmit
+TCTRL = TINIT = 0                           # Control frame and initialization random timeouts (in seconds)
+TMAX = 120                                  # Max time for network mode (in seconds)
+START_TIME = 0
+
+
+PLOAD_SIZE = 32                             # Payload size corresponding to data in one frame (32 B max)
+HDR_SIZE = 1                                # Header size inside payload frame
+
+# TRANSCEIVER CONSTANTS
+RF_CH = 0x64                        # UL & DL channels
+PWR_LVL = NRF24.PA_MIN                     # Transceiver output (HIGH = -6 dBm + 20 dB)
+BRATE = NRF24.BR_250KBPS                    # 250 kbps bit rate
+
+SEND_ACK1 = 0
+SEND_ACK2 = 0
+SEND_ACK3 = 0
+
+ACKED = {m.B_TEAM : 0, m.C_TEAM : 0, m.D_TEAM : 0}
+
+
 PIPES = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]  # addresses for TX/RX channels
+EARS_PIPE = [1]
+
+ACTIVE_TEAM = m.A_TEAM
+
+PKTS_RCVD = 0
+
+last_w_id_B = -1
+last_w_id_C = -1
+last_w_id_D = -1
+
+F_CMPLTD = 0
+# POS_MAX = 
+
+storedFrames = {"-2N": "DEFAULT"}  # NO ENTIENDO ESTO
+
+
 
 GPIO.setup([0,1,17,27], GPIO.OUT, initial=GPIO.LOW)
 ears = NRF24(GPIO, spidev.SpiDev())  # EARS
