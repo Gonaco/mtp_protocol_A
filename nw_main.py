c42c7f924ed789632fa8daef820ccdbc62fd0703
Gonaco <danielmoremanza@gmail.com>
2017-11-24 11:06:02 +0100

    NW tests

--- nw_main.py
+++ nw_main.py
@@ -5,24 +5,24 @@ ears, mouth = nw.setup()
 
 timer = 1
 
-# while True:
-#     if (nw.listen(ears, timer)):
-#         nw.passive(ears,mouth)
-#     else:
-#         print("Timeout")
-
 while True:
-    
-    # nw.active(ears,mouth)
-    message = "Christian putamo"
-    mouth.write(message)
-    print(message)
-    time.sleep(0.001)
     if (nw.listen(ears, timer)):
-        print ("ACK received")
-
+        nw.passive(ears,mouth)
     else:
         print("Timeout")
+
+# while True:
+    
+#     # nw.active(ears,mouth)
+#     message = "Christian putamo"
+#     mouth.write(message)
+#     print(message)
+#     time.sleep(0.001)
+#     if (nw.listen(ears, timer)):
+#         print ("ACK received")
+
+#     else:
+#         print("Timeout")
     
 
 # f = open("tx_file.txt", 'r')
