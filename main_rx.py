3c9bce0cd91b1c0e3e5cb150bfde5b1700c0fdbb
carlosargal <carlosargal@gmail.com>
2017-11-24 22:26:40 +0100

    rx_main_functions.py modified

--- main_rx.py
+++ main_rx.py
@@ -1,12 +1,16 @@
 import rx_main_functions as rx
-
+import time
 
 def main_rx():
+    start = time.time()
     radio, radio2 = rx.setup()
     pipe = [0]
     frame_received = rx.handshake(radio, radio2, pipe, 0)
     final_id = rx.receive(radio, radio2, pipe, frame_received)
     rx.handshake(radio, radio2, pipe, final_id)
+    end = time.time()
+    diff = end - start
+    print("Done sending the file! Exiting! It took: ", diff, " seconds")
     return 0
 
 if __name__ == "__main__":
